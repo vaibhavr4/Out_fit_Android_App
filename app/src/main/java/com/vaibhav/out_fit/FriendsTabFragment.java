c7b1f85fee13529dd18bedca92cc59ce3edba17d
vaibhav rangarajan <rvaibhav1793@gmail.com>
2019-07-17 11:49:54 -0400

    Merged outdoor with material tab

--- app/src/main/java/com/vaibhav/out_fit/FriendsTabFragment.java
+++ app/src/main/java/com/vaibhav/out_fit/FriendsTabFragment.java
@@ -6,10 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Tab2Fragment extends Fragment {
+public class FriendsTabFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_tab2_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_friends_layout, container, false);
     }
 }
