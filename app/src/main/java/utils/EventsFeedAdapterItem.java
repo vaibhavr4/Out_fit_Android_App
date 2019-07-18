03afaa1c9d1ed7e88047126c012e9d852ea9a53b
vaibhav rangarajan <rvaibhav1793@gmail.com>
2019-07-17 21:18:45 -0400

    Updated mock data for newsfeed

---
+++ app/src/main/java/utils/EventsFeedAdapterItem.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class EventsFeedAdapterItem {
+
+    String events;
+
+    public EventsFeedAdapterItem(String events) {
+        this.events = events;
+    }
+
+    public String getEvents() {
+        return events;
+    }
+
+    public void setEvents(String events) {
+        this.events = events;
+    }
+
+
+
+}
---
+++ app/src/main/java/utils/NewsFeedAdapterItem.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class NewsFeedAdapterItem {
+    String news;
+
+    public NewsFeedAdapterItem(String news) {
+        this.news = news;
+    }
+
+    public String getNews() {
+        return news;
+    }
+
+    public void setNews(String news) {
+        this.news = news;
+    }
+}
